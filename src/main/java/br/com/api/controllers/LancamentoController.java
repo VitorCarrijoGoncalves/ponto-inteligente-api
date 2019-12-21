b51c0a58efd4673408c15ad4df86ea18ea62c256
raonedaniel <vitorcg002@gmail.com>
2019-12-21 02:38:01 -0300

    Classe LancamentoDto implementada

---
+++ src/main/java/br/com/api/controllers/LancamentoController.java
@@ -0,0 +1,12 @@
+package br.com.api.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lancamentos")
+@CrossOrigin(origins = "*")
+public class LancamentoController {
+
+}
